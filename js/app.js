930d8666826e4dbf734bd48940fa01dc4d1fff0a
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-03-10 07:35:35 -0700

    Added five objects for each store and updated CSS files

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ var seattle = {
   customerArray: [],
   cookiesSoldArray: [],
 
-  seattleMetrics: function() {
+  metrics: function() {
     var totalCookiesSold = 0;
 
     for (var i = 0; i <= this.hoursOpen -1; i++) {
@@ -33,59 +33,292 @@ var seattle = {
       this.customerArray.push(randomCustomer);
       this.cookieArray.push(totalCookies);
       this.cookiesSoldArray.push(totalCookiesSold);
-
-      // console.log(this.cookieArray.reduce());
-
-      //Variables to getting the element to adjust
-      // var parentElement = document.getElementById('seattle');
-      // var childElement = document.createElement('li');
-
-      //Creates li and writes to each li element the cookies and a string
-      // parentElement.appendChild(childElement);
-      // childElement.textContent = hoursOfOperation[i] + this.cookieArray[i] + ' cookies';
-      // childElement.append();
-
-      // var result = [randomCustomer, totalCookies, totalCookiesSold];
-
     }
-
-    //Writes the total at the end of the list
-    // childElement.textContent = 'Total: ' + totalCookiesSold + ' cookies';
-    // childElement.append();
-    console.log(this.customerArray);
-    console.log(this.cookieArray);
-    console.log(totalCookiesSold);
-    // console.log(result);
   },
 
   renderData: function () {
 
+    //Runs metrics function for data
+    this.metrics();
+
     for (var i = 0; i < this.hoursOfOperation.length; i++) {
-    //Variables to write the data
+
+      //variables created parentEl gets the correct <ul> and childEl creates a <li> tag
       var parentElement = document.getElementById(this.name);
       var childElement = document.createElement('li');
 
-      //Create li and writes each li element, cookies, and string to html
+      //This appends or changes parentEl <ul>, by added new childEl <li>
       parentElement.appendChild(childElement);
-      // childElement.textContent = hoursOfOperation[i] + this.cookieArray[i] + ' Cookies';
+
+      // This is the actually string content placed inside <li> element
       childElement.textContent = this.hoursOfOperation[i] + this.cookieArray[i] + ' Cookies';
       childElement.append();
     }
 
     //This variable pulls the last running total of cookies sold
     var lastTotal = this.cookiesSoldArray[this.cookiesSoldArray.length -1];
-    console.log(lastTotal);
+    // console.log(lastTotal);
+
+    //Writes the final total cookies to bottom of list
     childElement.textContent = 'Total: ' + lastTotal + ' Cookies';
   }
 };
 
+var tokyo = {
+  name: 'tokyo',
+  min: 3,
+  max: 24,
+  average: 1.2,
+  hoursOpen: 14,
+  hoursOfOperation: ['7am: ', '8am ','9am ', '10am ', '11am ', '12pm ', '1pm ', '2 pm ', '3pm ', '4pm ', '5pm ', '6pm ', '7pm ', '8pm ' ],
+  cookieArray: [],
+  customerArray: [],
+  cookiesSoldArray: [],
 
-seattle.seattleMetrics();
+  metrics: function() {
+    var totalCookiesSold = 0;
+
+    for (var i = 0; i <= this.hoursOpen -1; i++) {
+      // for (var i= 0; i < 1; i++) {
+      //Calculates random number between min and max customers
+      var randomCustomer = Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+      // console.log(randomCustomer);
+
+      //Takes randomCustomer value times the average cookie sale
+      var totalCookies = Math.floor(randomCustomer * this.average);
+      // console.log(totalCookies);
+
+      //Running total of cookies sold per hour (hours are calculated in the condition of the loop)
+      totalCookiesSold = totalCookiesSold + totalCookies;
+      // console.log(totalCookiesSold);
+
+      //stores all data into two arrays
+      this.customerArray.push(randomCustomer);
+      this.cookieArray.push(totalCookies);
+      this.cookiesSoldArray.push(totalCookiesSold);
+    }
+  },
+
+  renderData: function () {
+
+    //Runs metrics function for data
+    this.metrics();
+
+    for (var i = 0; i < this.hoursOfOperation.length; i++) {
+
+      //variables created parentEl gets the correct <ul> and childEl creates a <li> tag
+      var parentElement = document.getElementById(this.name);
+      var childElement = document.createElement('li');
+
+      //This appends or changes parentEl <ul>, by added new childEl <li>
+      parentElement.appendChild(childElement);
+
+      // This is the actually string content placed inside <li> element
+      childElement.textContent = this.hoursOfOperation[i] + this.cookieArray[i] + ' Cookies';
+      childElement.append();
+    }
+
+    //This variable pulls the last running total of cookies sold
+    var lastTotal = this.cookiesSoldArray[this.cookiesSoldArray.length -1];
+    // console.log(lastTotal);
+
+    //Writes the final total cookies to bottom of list
+    childElement.textContent = 'Total: ' + lastTotal + ' Cookies';
+  }
+};
+
+var dubai = {
+  name: 'dubai',
+  min: 11,
+  max: 38,
+  average: 3.7,
+  hoursOpen: 14,
+  hoursOfOperation: ['7am: ', '8am ','9am ', '10am ', '11am ', '12pm ', '1pm ', '2 pm ', '3pm ', '4pm ', '5pm ', '6pm ', '7pm ', '8pm ' ],
+  cookieArray: [],
+  customerArray: [],
+  cookiesSoldArray: [],
+
+  metrics: function() {
+    var totalCookiesSold = 0;
+
+    for (var i = 0; i <= this.hoursOpen -1; i++) {
+      // for (var i= 0; i < 1; i++) {
+      //Calculates random number between min and max customers
+      var randomCustomer = Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+      // console.log(randomCustomer);
+
+      //Takes randomCustomer value times the average cookie sale
+      var totalCookies = Math.floor(randomCustomer * this.average);
+      // console.log(totalCookies);
+
+      //Running total of cookies sold per hour (hours are calculated in the condition of the loop)
+      totalCookiesSold = totalCookiesSold + totalCookies;
+      // console.log(totalCookiesSold);
+
+      //stores all data into two arrays
+      this.customerArray.push(randomCustomer);
+      this.cookieArray.push(totalCookies);
+      this.cookiesSoldArray.push(totalCookiesSold);
+    }
+  },
+
+  renderData: function () {
+
+    //Runs metrics function for data
+    this.metrics();
+
+    for (var i = 0; i < this.hoursOfOperation.length; i++) {
+
+      //variables created parentEl gets the correct <ul> and childEl creates a <li> tag
+      var parentElement = document.getElementById(this.name);
+      var childElement = document.createElement('li');
+
+      //This appends or changes parentEl <ul>, by added new childEl <li>
+      parentElement.appendChild(childElement);
+
+      // This is the actually string content placed inside <li> element
+      childElement.textContent = this.hoursOfOperation[i] + this.cookieArray[i] + ' Cookies';
+      childElement.append();
+    }
+
+    //This variable pulls the last running total of cookies sold
+    var lastTotal = this.cookiesSoldArray[this.cookiesSoldArray.length -1];
+    // console.log(lastTotal);
+
+    //Writes the final total cookies to bottom of list
+    childElement.textContent = 'Total: ' + lastTotal + ' Cookies';
+  }
+};
+
+var paris = {
+  name: 'paris',
+  min: 20,
+  max: 38,
+  average: 2.3,
+  hoursOpen: 14,
+  hoursOfOperation: ['7am: ', '8am ','9am ', '10am ', '11am ', '12pm ', '1pm ', '2 pm ', '3pm ', '4pm ', '5pm ', '6pm ', '7pm ', '8pm ' ],
+  cookieArray: [],
+  customerArray: [],
+  cookiesSoldArray: [],
+
+  metrics: function() {
+    var totalCookiesSold = 0;
+
+    for (var i = 0; i <= this.hoursOpen -1; i++) {
+      // for (var i= 0; i < 1; i++) {
+      //Calculates random number between min and max customers
+      var randomCustomer = Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+      // console.log(randomCustomer);
+
+      //Takes randomCustomer value times the average cookie sale
+      var totalCookies = Math.floor(randomCustomer * this.average);
+      // console.log(totalCookies);
+
+      //Running total of cookies sold per hour (hours are calculated in the condition of the loop)
+      totalCookiesSold = totalCookiesSold + totalCookies;
+      // console.log(totalCookiesSold);
+
+      //stores all data into two arrays
+      this.customerArray.push(randomCustomer);
+      this.cookieArray.push(totalCookies);
+      this.cookiesSoldArray.push(totalCookiesSold);
+    }
+  },
+
+  renderData: function () {
+
+    //Runs metrics function for data
+    this.metrics();
+
+    for (var i = 0; i < this.hoursOfOperation.length; i++) {
+
+      //variables created parentEl gets the correct <ul> and childEl creates a <li> tag
+      var parentElement = document.getElementById(this.name);
+      var childElement = document.createElement('li');
+
+      //This appends or changes parentEl <ul>, by added new childEl <li>
+      parentElement.appendChild(childElement);
+
+      // This is the actually string content placed inside <li> element
+      childElement.textContent = this.hoursOfOperation[i] + this.cookieArray[i] + ' Cookies';
+      childElement.append();
+    }
+
+    //This variable pulls the last running total of cookies sold
+    var lastTotal = this.cookiesSoldArray[this.cookiesSoldArray.length -1];
+    // console.log(lastTotal);
+
+    //Writes the final total cookies to bottom of list
+    childElement.textContent = 'Total: ' + lastTotal + ' Cookies';
+  }
+};
+
+var lima = {
+  name: 'lima',
+  min: 2,
+  max: 16,
+  average: 4.6,
+  hoursOpen: 14,
+  hoursOfOperation: ['7am: ', '8am ','9am ', '10am ', '11am ', '12pm ', '1pm ', '2 pm ', '3pm ', '4pm ', '5pm ', '6pm ', '7pm ', '8pm ' ],
+  cookieArray: [],
+  customerArray: [],
+  cookiesSoldArray: [],
+
+  metrics: function() {
+    var totalCookiesSold = 0;
+
+    for (var i = 0; i <= this.hoursOpen -1; i++) {
+      // for (var i= 0; i < 1; i++) {
+      //Calculates random number between min and max customers
+      var randomCustomer = Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+      // console.log(randomCustomer);
+
+      //Takes randomCustomer value times the average cookie sale
+      var totalCookies = Math.floor(randomCustomer * this.average);
+      // console.log(totalCookies);
+
+      //Running total of cookies sold per hour (hours are calculated in the condition of the loop)
+      totalCookiesSold = totalCookiesSold + totalCookies;
+      // console.log(totalCookiesSold);
+
+      //stores all data into two arrays
+      this.customerArray.push(randomCustomer);
+      this.cookieArray.push(totalCookies);
+      this.cookiesSoldArray.push(totalCookiesSold);
+    }
+  },
+
+  renderData: function () {
+
+    //Runs metrics function for data
+    this.metrics();
+
+    for (var i = 0; i < this.hoursOfOperation.length; i++) {
+
+      //variables created parentEl gets the correct <ul> and childEl creates a <li> tag
+      var parentElement = document.getElementById(this.name);
+      var childElement = document.createElement('li');
+
+      //This appends or changes parentEl <ul>, by added new childEl <li>
+      parentElement.appendChild(childElement);
+
+      // This is the actually string content placed inside <li> element
+      childElement.textContent = this.hoursOfOperation[i] + this.cookieArray[i] + ' Cookies';
+      childElement.append();
+    }
+
+    //This variable pulls the last running total of cookies sold
+    var lastTotal = this.cookiesSoldArray[this.cookiesSoldArray.length -1];
+    // console.log(lastTotal);
+
+    //Writes the final total cookies to bottom of list
+    childElement.textContent = 'Total: ' + lastTotal + ' Cookies';
+  }
+};
+
 seattle.renderData();
+tokyo.renderData();
+dubai.renderData();
+paris.renderData();
+lima.renderData();
 
-console.log(seattle.cookiesSoldArray);
-
-
-// //Takes random customer and total cookies sold put them into an array
-// var result = [randomCustomer, totalCookies, totalCookieSales];
-// // console.log(result);
